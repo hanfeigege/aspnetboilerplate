10f54c6d27e828c5f3c1342bc49fe21fad92804a
hanfeigege <hanfeigege@outlook.com>
2016-08-07 10:04:42 +0800

    JsonExtensions Use Deault Settings
    see the code

--- src/Abp/Json/JsonExtensions.cs
+++ src/Abp/Json/JsonExtensions.cs
@@ -11,6 +11,24 @@ namespace Abp.Json
         /// <returns></returns>
         public static string ToJsonString(this object obj, bool camelCase = false, bool indented = false)
         {
+            var options = JsonConvert.DefaultSettings() ?? new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                DateFormatString = "yyyy-MM-dd hh:mm:ss",
+            };
+            if (camelCase)
+            {
+                options.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            }
+            if (indented)
+            {
+                options.Formatting = Formatting.Indented;
+            }
+            options.Converters.Insert(0, new AbpDateTimeConverter());
+            return JsonConvert.SerializeObject(value, options);
+            
+            //change this
+            
             var options = new JsonSerializerSettings();
 
             if (camelCase)
